420a0ba38696b15641ea3d437cfbe28ecd998fc7
Deff <44276186+deff-dev@users.noreply.github.com>
2024-07-07 12:59:41 +0300

    Update entrypoint.sh

--- source/entrypoint.sh
+++ source/entrypoint.sh
@@ -98,5 +98,5 @@ echo ":/home/container$ ${MODIFIED_STARTUP}"
 # Run the Server
  bash -c "
      unbuffer -p stdbuf --output 0 --error 0 ${MODIFIED_STARTUP}; 
-     exit $?
+     exit 'Server crashing'
    " 2>&1|  tee  "logs/console/test3_console.log" 
