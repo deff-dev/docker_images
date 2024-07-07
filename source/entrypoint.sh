0a650e999669811575da60ec2361dd8b7465a3c7
Deff <44276186+deff-dev@users.noreply.github.com>
2024-07-07 12:05:19 +0300

    Update entrypoint.sh

--- source/entrypoint.sh
+++ source/entrypoint.sh
@@ -96,7 +96,7 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-stdbuf --output 0 --error 0 
+unbuffer -p
   bash -c "
       ${MODIFIED_STARTUP}; 
      exit $?
