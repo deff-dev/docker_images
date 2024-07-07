292772da44af702df746b989eef650ef118ef925
Deff <44276186+deff-dev@users.noreply.github.com>
2024-07-07 13:28:49 +0300

    Update entrypoint.sh

--- source/entrypoint.sh
+++ source/entrypoint.sh
@@ -97,5 +97,5 @@ echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
  bash -c "
-     script -q -c '${MODIFIED_STARTUP}' logs/console/test5_console.log; 
+     script -q -c '${MODIFIED_STARTUP}' 
    " 2>&1|  tee  "logs/console/test3_console.log" 
