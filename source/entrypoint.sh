1e9e06445191583b117aa11e6e27d322d8ee2028
Deff <44276186+deff-dev@users.noreply.github.com>
2024-07-07 09:18:24 +0300

    Update entrypoint.sh

--- source/entrypoint.sh
+++ source/entrypoint.sh
@@ -96,4 +96,4 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-stdbuf -o0 ${MODIFIED_STARTUP} | tee "logs/console/${DATE}_console.log"
+unbuffer -p ${MODIFIED_STARTUP} | tail -f "logs/console/${DATE}_console.log"
