71d9f53af1640bb722a39c8b182521bfbc776411
Deff <44276186+deff-dev@users.noreply.github.com>
2024-07-23 12:37:00 +0300

    Test fix steamcmd

--- source/entrypoint.sh
+++ source/entrypoint.sh
@@ -60,7 +60,18 @@ fi
 if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
     # Update Source Server
     if [ ! -z ${SRCDS_APPID} ]; then
-        ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
+        counter=0
+        exitcode=0
+        while [ "${counter}" == "0" ] || [ "${exitcode}" != "0" ]; do
+            counter=$((counter + 1))
+            ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
+            exitcode=$?
+            if [ "${counter}" -gt "10" ]; then
+            #    mkdir -p game/csgo/backup && cp game/csgo/cfg/* game/csgo/backup/
+                echo rm -rf steamapps/downloading/*
+                break
+            fi
+        done
     else
         echo -e "No appid set. Starting Server"
     fi
